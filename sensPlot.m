303afb0310249179707c089fa461ed57088dbb88
Kyoshiro Kamura <reallove1970@gmail.com>
2021-09-13 15:01:29 +0900

    add link

--- sensPlot.m
+++ sensPlot.m
@@ -4,9 +4,50 @@ close all
 status = system("scp raspi:~/bme280/data.csv data.csv");
 
 filename = 'data.csv';
-A = readcell(filename);
+data = readcell(filename);
 
-time = A{:, 1};
-temp = A{:, 2};
-humi = A{:, 3};
-pres = A{:, 4};
+time = datetime(string((data(:, 1))));
+temp = cell2mat(data(:, 2));
+humi = cell2mat(data(:, 3));
+pres = cell2mat(data(:, 4));
+
+f = figure;
+f.Position(2:4) = [200 750 750];
+fs = 12;
+n = 3;
+subplot(n, 1, 1)
+plot(time, temp, '-ok')
+str =...
+    strcat(string(min(month(time))), "/", string(max(day(time))-1),...
+    "                                        Time                                         ",...
+    string(max(month(time))), "/", string(max(day(time))));
+xlabel(str)
+xlim([time(end)-hours(24) time(end)])
+xticks(time)
+xticklabels(string(hour(time)))
+ylabel('Temperature in degree')
+ylim([-10 40])
+grid on
+set(gca, 'FontSize', fs)
+
+subplot(n, 1, 2)
+plot(time, humi, '-ok')
+xlabel(str)
+xlim([time(end)-hours(24) time(end)])
+xticks(time)
+xticklabels(string(hour(time)))
+ylabel('Humidity in %')
+ylim([40 90])
+grid on
+set(gca, 'FontSize', fs)
+
+subplot(n, 1, 3)
+plot(time, pres, '-ok')
+xlim([time(end)-hours(24) time(end)])
+xticks(time)
+xticklabels(string(hour(time)))
+xlabel(str)
+ylabel('Pressure in hPa')
+ylim([1000 1020])
+grid on
+set(gca, 'FontSize', fs)
